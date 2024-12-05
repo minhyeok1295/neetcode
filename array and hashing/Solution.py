9cf91d14ac74d534237551757228f008a81bd64b
Min Hyeok Lee <min.hyeok.lee@firsthx.com>
2024-12-05 16:10:53 -0500

    new soln added

--- array and hashing/Solution.py
+++ array and hashing/Solution.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import List
 
 
@@ -73,3 +74,21 @@ class Solution:
             suffix *= nums[i]
         
         return result
+    
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        boxes = defaultdict(set)
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    continue
+                val = board[i][j]
+                box_index = (i // 3) * 3 + j // 3
+                if val in rows[i] or val in cols[j] or val in boxes[box_index]:
+                    return False
+                rows[i].add(val)
+                cols[j].add(val)
+                boxes[box_index].add(val)
+        return True
